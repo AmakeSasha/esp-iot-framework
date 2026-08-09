456dcc4a7cde4aa2e227d25173a9b0416449cb33
AmakeSasha <Amakesasha@yandex.ru>
2026-08-10 01:29:24 +0500

    Delete chapter 'LED' from 'esp_iot_framework_core_ext.h'

--- docs/html/group__core__ext__group.js
+++ docs/html/group__core__ext__group.js
@@ -2,7 +2,6 @@ var group__core__ext__group =
 [
     [ "Constants", "group__core__ext__consts.html", "group__core__ext__consts" ],
     [ "Tools", "group__core__ext__tools.html", "group__core__ext__tools" ],
-    [ "LED pin", "group__core__ext__led__pins.html", "group__core__ext__led__pins" ],
     [ "Configuration", "group__core__ext__config.html", "group__core__ext__config" ],
     [ "Event Handlers", "group__core__ext__handlers.html", "group__core__ext__handlers" ],
     [ "Non-Volatile Storage (NVS)", "group__core__ext__nvs.html", "group__core__ext__nvs" ],
